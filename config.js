f39144c04a4d99a539185f320037aa26e5674fb9
Adrian Davila <a4davila@uwaterloo.ca>
2022-04-24 19:00:31 -0400

    Removed about me from config - easier directly in component

--- config.js
+++ config.js
@@ -6,12 +6,6 @@ module.exports = {
     paragraph: "Note: I want to create for myself a similar background as this: https://gridcritters.com/ - (I also have a screenshot on my desktop). Can also use this galaxy possibly: https://adobe.ly/3rFvtLJ . Will use same text I have from Brittany Chang Website. Desired navbar: https://www.youtube.com/watch?v=HbBMp6yUXO0",
   },
 
-  /* ------------- ABOUT ME ------------- */
-  about: {
-    desc: "",
-    technologies: [],
-  },
-
   /* ------------- MY SOCIALS ------------- */
   email: 'a4davila@uwaterloo.ca',
   resume: '',
